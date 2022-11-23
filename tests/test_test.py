6688e6576f5303f7f6df772132c5407fa08b4b92
Pepperonix <61380904+DenizTanriverdi@users.noreply.github.com>
2022-11-23 16:32:10 +0100

    add tests

---
+++ tests/test_test.py
@@ -0,0 +1,12 @@
+from tests.basetest import Basetest
+
+class TestWikidataSearch(Basetest):
+    """
+    test the wikidata search
+    """
+    
+    def test_wikidata_search(self):
+        print("hallo")
+        self.assertEqual(2,2)
+        pass
+    
